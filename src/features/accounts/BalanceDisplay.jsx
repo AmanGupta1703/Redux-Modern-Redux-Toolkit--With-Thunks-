5459716b6fc2a7e67b8a3403f7d4e3a9a49e5285
AmanGupta1703 <guptakunalx@gmail.com>
2024-02-08 19:24:32 +0530

    Refactor BalanceDisplay component to use react-redux hooks (legacy approach and new approach)

--- src/features/accounts/BalanceDisplay.jsx
+++ src/features/accounts/BalanceDisplay.jsx
@@ -1,12 +1,28 @@
+import { useSelector } from "react-redux";
+
 function formatCurrency(value) {
-	return new Intl.NumberFormat("en", {
-		style: "currency",
-		currency: "USD",
-	}).format(value);
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
 }
 
 function BalanceDisplay() {
-	return <div className="balance">{formatCurrency(123456)}</div>;
+  const { balance } = useSelector(state => state.account);
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
 export default BalanceDisplay;
+
+/***
+ * // LEGACY APPROACH
+ * function BalanceDisplay({ balance }) {
+ * 	return <div className="balance">{formatCurrency(balance)}</div>
+ * }
+ * function mapStateToProps(state) {
+ * 	return {
+ * 		balance: state.account.balance,
+ * 	};
+ * }
+ * export default connect(mapStateToProps)(BalanceDisplay);
+ */
