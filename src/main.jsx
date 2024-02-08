2e8d6deea1ec9c1e55687b3fdc0f3bfd4edda3c8
AmanGupta1703 <guptakunalx@gmail.com>
2024-02-08 17:14:05 +0530

    Add react-redux dependency and update Customer component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import App from "./App.jsx";
 import "./index.css";
 
 import store from "./store.js";
 
-store.dispatch({ type: "account/deposit", payload: 50_000_000_000 });
-console.log(store.getState());
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
+/***
+ * Using `Provider` means that every component can read
+ * data from the store or dispatch actions to the store.
+ * (similar to Context API)
+ */
