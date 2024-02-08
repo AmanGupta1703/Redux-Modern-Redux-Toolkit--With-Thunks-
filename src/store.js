9cd7f03cc6fc7d414b11a6108ef254d082b1e5d0
AmanGupta1703 <guptakunalx@gmail.com>
2024-02-08 15:59:32 +0530

    Add combineReducers and create customer state reducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -34,7 +40,31 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateCustomer":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+// createstore accepts a single argument i.e root reducer,
+// to combine all the reducer and
+// and create a root reducer
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 function deposit(amount) {
   return { type: "account/deposit", payload: amount };
@@ -62,6 +92,21 @@ store.dispatch(payLoan());
 
 console.log(store.getState());
 
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateCustomer(fullName) {
+  return { type: "customer/updateCustomer", payload: fullName };
+}
+
+store.dispatch(createCustomer("Aman Gupta", "5628"));
+
+console.log(store.getState());
+
 /***
  * Steps:
  * 1) Create the initial state, and create a reducer function
@@ -69,4 +114,6 @@ console.log(store.getState());
  * 3) import createStore @deprecated
  * 4) initialize a store
  * 5) create action creator functions.
+ * 6) same steps for Customer state
+ *
  */
