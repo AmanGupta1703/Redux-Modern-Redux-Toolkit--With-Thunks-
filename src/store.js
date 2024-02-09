be8add334f24839bc5e2593ee050c41d6aba5b3b
AmanGupta1703 <guptakunalx@gmail.com>
2024-02-09 11:09:06 +0530

    Add Redux DevTools extension and update store configuration

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
@@ -12,7 +13,10 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
 
